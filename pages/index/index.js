4e73897fd1e37a2724b5cdad1c70b4df81d181c1
swartcore <12920@qq.com>
2026-06-14 13:33:35 +0800

    Refine home carousel and service cards

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,23 @@ const api = require('../../utils/api.js')
 
 Page({
   data: {
-    posters: [
+    heroReady: false,
+    posters: shuffle([
       `${api.ASSET_URL}/poster-home-3x4.png`,
       `${api.ASSET_URL}/poster-service-500-3x4.png`
-    ]
+    ])
+  },
+  onPosterLoad(e) {
+    if (Number(e.currentTarget.dataset.index) === 0 && !this.data.heroReady) {
+      this.setData({ heroReady: true })
+    }
+  },
+  onPosterError() {
+    this.setData({ heroReady: true })
+    wx.showToast({
+      title: '海报加载失败',
+      icon: 'none'
+    })
   },
   goService() {
     wx.navigateTo({
@@ -19,3 +32,14 @@ Page({
     })
   },
 })
+
+function shuffle(list) {
+  const result = list.slice()
+  for (let index = result.length - 1; index > 0; index -= 1) {
+    const swapIndex = Math.floor(Math.random() * (index + 1))
+    const temp = result[index]
+    result[index] = result[swapIndex]
+    result[swapIndex] = temp
+  }
+  return result
+}
