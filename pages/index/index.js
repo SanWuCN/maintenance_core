ad92383cd85e012b0616042afcbc7ee4fdd0258b
swartcore <12920@qq.com>
2026-06-14 14:33:58 +0800

    Use layered tech cards with uploaded icons

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,14 @@ Page({
   data: {
     heroReady: false,
     bootLogo: `${api.ASSET_URL}/app-icon-boot-transparent.png`,
+    businessIcons: {
+      clean: `${api.ASSET_URL}/service-fan-icon.png`,
+      upgrade: `${api.ASSET_URL}/service-chip-icon.png`,
+      optimize: `${api.ASSET_URL}/service-gear-icon.png`,
+      cleanWhite: `${api.ASSET_URL}/service-fan-icon-white.png`,
+      upgradeWhite: `${api.ASSET_URL}/service-chip-icon-white.png`,
+      optimizeWhite: `${api.ASSET_URL}/service-gear-icon-white.png`
+    },
     posters: []
   },
   onLoad() {
